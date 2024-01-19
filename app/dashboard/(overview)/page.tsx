b3ca9e2003f523732d3d6144b1b328f959d390ef
SmalWriter <a0988500558@gmail.com>
2024-01-19 14:28:42 +0800

    Remove unused code. (generated by blackbox.ai )

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,21 +10,13 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 
-export default async function Page() {
-
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // }  
+export default async function Page() { 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-       
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
       <Suspense fallback={<CardsSkeleton />}>
